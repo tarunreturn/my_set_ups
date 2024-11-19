6258da1876890a8862f50571b1c55ab87b382d6b
Tarun <141718214+tarunreturn@users.noreply.github.com>
2024-11-19 20:06:42 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -30,7 +30,7 @@ aws s3api put-bucket-versioning --bucket $S3_BUCKET_NAME --region $REGION --vers
 export KOPS_STATE_STORE=s3://$S3_BUCKET_NAME
 
 # Create the Kubernetes cluster with explicit cloud provider (AWS)
-kops create cluster --name $CLUSTER_NAME --zones ${REGION}a --cloud aws --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.medium
+kops create cluster --name $CLUSTER_NAME --zones ${REGION}a --cloud aws --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
 
 # Apply the changes to the cluster
 kops update cluster --name $CLUSTER_NAME --yes --admin
