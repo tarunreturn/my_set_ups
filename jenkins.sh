48f5c1725163ead94befbfc41e4bca868eac8731
Tarun <141718214+tarunreturn@users.noreply.github.com>
2024-11-28 15:34:57 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,11 +1,12 @@
-#STEP-1:installing git maven java
-yum install git maven java-1.8.0 -y
+#use global tools config in jenkins for maven to avoid java releted problems
+#STEP-1:installing git  java
+yum install git -y
 #STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 #STEP-3: DOWNLOAD JAVA AND JENKINS
 yum install jenkins java-17 -y
-update-alternatives --config java
+#update-alternatives --config java
 #STEP-4: RESTARTING JENKINS (when we download service it will on stopped state)
 systemctl start jenkins
 chkconfig jenkins on
