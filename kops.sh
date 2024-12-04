12942a4ce80a547daa433fe32b982753e4794f53
Tarun <141718214+tarunreturn@users.noreply.github.com>
2024-12-04 10:05:59 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -1,36 +1,18 @@
-#run this commands mannual if run on linux 
 #vim .bashrc
 #export PATH=$PATH:/usr/local/bin/
 #source .bashrc
-#!/bin/bash
-# Prompt user for input
-read -p "Enter your AWS region (eg:us-east-1): " REGION
-read -p "Enter your S3 bucket name (e.g., mys3.k8s.local): " S3_BUCKET_NAME
-read -p "Enter your cluster name (e.g., mycluster.k8s.local): " CLUSTER_NAME
 
-# AWS Configuration or if u use IAM role ingnore it
+
+#! /bin/bash
 aws configure
-
-# Download kubectl and kops
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 wget https://github.com/kubernetes/kops/releases/download/v1.25.0/kops-linux-amd64
-
-# Make the binaries executable
 chmod +x kops-linux-amd64 kubectl
-
-# Move the binaries to /usr/local/bin
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-# Create S3 bucket with versioning enabled
-aws s3api create-bucket --bucket $S3_BUCKET_NAME --region $REGION
-aws s3api put-bucket-versioning --bucket $S3_BUCKET_NAME --region $REGION --versioning-configuration Status=Enabled
-
-# Set KOPS_STATE_STORE environment variable
-export KOPS_STATE_STORE=s3://$S3_BUCKET_NAME
-
-# Create the Kubernetes cluster with explicit cloud provider (AWS)
-kops create cluster --name $CLUSTER_NAME --zones ${REGION}a --cloud aws --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
-
-# Apply the changes to the cluster
-kops update cluster --name $CLUSTER_NAME --yes --admin
+aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
+kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+kops update cluster --name rahams.k8s.local --yes --admin
