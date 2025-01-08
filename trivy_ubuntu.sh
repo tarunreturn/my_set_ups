0cdf02e9335053bf7006b57cfe45ff96656c5cae
Tarun <141718214+tarunreturn@users.noreply.github.com>
2025-01-08 18:31:50 +0530

    Update trivy_ubuntu.sh

--- trivy_ubuntu.sh
+++ trivy_ubuntu.sh
@@ -1,4 +1,4 @@
-#!bin
+#! /bin/bash
 sudo apt-get install wget apt-transport-https gnupg lsb-release
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
