d6e9bc5a4a1508fe4d195c492b56ce515c6903e7
Tarun <141718214+tarunreturn@users.noreply.github.com>
2025-01-11 14:20:41 +0530

    Create EKS.sh

---
+++ EKS.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+#AWSCLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+unzip awscliv2.zip
+sudo ./aws/install
+#KUBECTL
+curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin
+kubectl version --short --client
+#EKSCTL
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
